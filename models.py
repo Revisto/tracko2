c2175fed212ab40a2bdc732d3f26cf21aeed3655
revisto <walt.shabani@gmail.com>
2021-04-06 17:27:49 +0430

    create class Rich

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import questionary
-
-
+import rich
+from rich.console import Console
 class Questionary:
     def ask_for_text(self, question):
         answer = questionary.text(question).ask()
@@ -21,3 +21,20 @@ class Questionary:
     def ask_checkbox_question(self, question, choices: list):
         answer = questionary.checkbox(question, choices=choices).ask()
         return answer
+
+
+class Rich:
+    def rich_print(self, text):
+        rich.print(text)
+
+
+    def table(self, columns:list, rows:list):
+        console = Console()
+        table = rich.table.Table(show_header=True, header_style="bold magenta")
+        for column in columns:
+            table.add_column(column)
+        for row in rows:
+            table.add_row(
+                *row
+            )
+        console.print(table)
